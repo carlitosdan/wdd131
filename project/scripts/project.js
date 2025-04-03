6f4e75616e6a55e26f2a1075c29561b446e7b1c3
carlitosdan <cardafma@gmail.com>
2025-04-03 15:29:48 -0500

    w5

---
+++ project/scripts/project.js
@@ -0,0 +1,14 @@
+// const hamButton = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation');
+
+// hamButton.addEventListener('click', () => {
+// 	navigation.classList.toggle('open');
+// 	hamButton.classList.toggle('open');
+// });
+
+
+d = new Date();
+year = d.getFullYear();
+document.getElementById("year").innerHTML = year;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
